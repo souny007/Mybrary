d520dd631cb18c5bdc735cb34299f12e0710a251
souny <sounymlv@gmail.com>
2019-11-03 10:04:00 +0700

    Test Delete

--- routes/authors.js
+++ routes/authors.js
@@ -1,41 +1,103 @@
 const express = require('express');
 const router = express.Router();
 const Author = require('../models/author');
+const Book = require('../models/book')
 
 //All authors router
-    router.get('/', async (req, res) => {
-        let searchOptions = {}
-    if(req.query.name != null && req.query.name !== ''){
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if (req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
-        try{
-            const authors = await Author.find(searchOptions)
-            res.render('authors/index', {
-                authors: authors,
-                searchOptions: req.query})
-        }catch{
-            res.redirect('/')
-        }
-    })
+    try {
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors,
+            searchOptions: req.query
+        })
+    } catch{
+        res.redirect('/')
+    }
+})
 //new authors router
-router.get('/new', function(req,res){
-    res.render('authors/new',{ author: new Author()})
+router.get('/new', function (req, res) {
+    res.render('authors/new', { author: new Author() })
 });
 //Create author route
-router.post('/', async (req, res)=>{
+router.post('/', async (req, res) => {
 
     const author = new Author({
         name: req.body.name
     })
-    try{
+    try {
         const newAuthor = await author.save()
-    // res.redirect(`authors/${newAuthor.id}`)
-        res.redirect('authors')
-    }catch(err){
-        res.render('authors/new',{
-                author: author,
-                errorMessage: 'Error Creating Author'
+        res.redirect(`authors/${newAuthor.id}`)
+    } catch (err) {
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error Creating Author'
         })
+    }
+})
+//show
+router.get('/:id', async (req, res) => {
+    
+    try {
+        const author = await Author.findById(req.params.id)
+         const books = await Book.find({author: author.id}).limit(6).exec()
+        res.render('authors/show',{
+            author:author,
+            bookByAuthor: books
+        })
+    } catch (err) {
+        console.log(err)
+        res.redirect('/')
+    }
+})
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const author = await Author.findById(req.params.id)
+        res.render('authors/edit', { author: author })
+    } catch{
+        res.redirect('/authors')
+    }
+
+})
+//update
+router.put('/:id', async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        author.name = req.body.name
+        await author.save()
+        res.redirect(`/authors/${author.id}`)
+        res.redirect('authors')
+    } catch (err) {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.render('authors/edit', {
+                author: author,
+                errorMessage: 'Error Update Author'
+            })
+        }
+
+    }
+})
+//delete
+router.delete('/:id', async (req, res) => {
+    let author
+    try {
+        author = await Author.findById(req.params.id)
+        await author.remove()
+        res.redirect('/authors')
+    } catch (err) {
+        if (author == null) {
+            res.redirect('/')
+        } else {
+            res.redirect(`/authors/${author.id}`)
+        }
+
     }
 })
 module.exports = router
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production'){
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
@@ -27,6 +28,7 @@ app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
 app.use(express.static('public'));
+app.use(methodOverride('_method'))
 
 //setting router
 const indexRouter = require('./routes/index');// ເອີ້ນໃຊ້ routes page
